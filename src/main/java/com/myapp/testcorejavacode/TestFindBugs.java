c64314c2ba8de260bd3935194c7bdf320dadb5b6
chhayanikam <chhaya.nikam@gmail.com>
2020-09-01 18:14:31 +0530

    Myfirst commit

---
+++ src/main/java/com/myapp/testcorejavacode/TestFindBugs.java
@@ -0,0 +1,13 @@
+package com.myapp.testcorejavacode;
+import java.util.*;
+
+public class TestFindBugs {
+public void FindBugMethod()
+{
+	List list=new ArrayList<String>();
+	String input1=null;
+	String input2=null;
+	input1.length();
+}
+	
+}
